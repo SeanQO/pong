55b8c610319a419a28630819e690f452daae6f2c
SeanQO <seanquintero3@gmail.com>
2022-06-23 21:54:31 -0500

    added push library to add a fixed resolution to the game, added quit key bind as 'escape' key

--- main.lua
+++ main.lua
@@ -1,10 +1,25 @@
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+-- https://github.com/Ulydev/push
+push = require 'push'
 
+--[[
+    game real window is reduced to present the game in window mode with 80% the size of the user screen
+]]
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
+WINDOW_WIDTH = windowWidth*.8
+WINDOW_HEIGHT = windowHeight*.8
+
+--[[
+    game actual render window size, set to give retro vibes at a lower quality( values are the original pong resolution 858x525)
+]]
+VIRTUAL_WIDTH = 858
+VIRTUAL_HEIGHT = 525
+
+local n 
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
-        resizable = false, 
+        resizable = false,
         vsync = true
     })
     n = 0
@@ -15,16 +30,28 @@ function love.update(dt)
 end
 
 function love.draw()
+    push:apply('start')
     love.graphics.printf(
         'PONG',
         0,
-        WINDOW_HEIGHT / 2 -6,
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT / 2 -6,
+        VIRTUAL_WIDTH,
         'center'
     )
-    love.graphics.print(
-        'n:' .. n,
+
+    nString = 'n:' .. n
+    love.graphics.printf(
+        nString,
         8,
-        4
+        4,
+        VIRTUAL_WIDTH,
+        'left'
     )
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
