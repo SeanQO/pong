1b91643d61251b686a33af9711bfa95531160305
SeanQO <seanquintero3@gmail.com>
2022-06-23 23:19:09 -0500

    improved code documentation

--- main.lua
+++ main.lua
@@ -1,23 +1,29 @@
--- https://github.com/Ulydev/push
+--[[
+    https://github.com/Ulydev/push
+    'push is a simple resolution-handling library that allows you to focus on making your game with a fixed resolution.'
+    push library is used to render a game with a fixed virtual resolution. The desired retro low res render is keept instead of however large the user window is. 
+]]
 push = require 'push'
 
---[[
-    game real window is reduced to present the game in window mode with 80% the size of the user screen
-]]
+--game real window is reduced to present the game in window mode with 80% the size of the user screen
 local windowWidth, windowHeight = love.window.getDesktopDimensions()
 WINDOW_WIDTH = windowWidth*.8
 WINDOW_HEIGHT = windowHeight*.8
 
---[[
-    game actual render window size, set to give retro vibes at a lower quality( values are the original pong resolution 858x525)
-]]
+--game actual render window size, set to give retro vibes at a lower quality( values are the original pong resolution 858x525)
 VIRTUAL_WIDTH = 858
 VIRTUAL_HEIGHT = 525
 
+--[[
+    https://www.dafont.com/es/bitmap.php
+    https://dl.dafont.com/dl/?f=pixbob_lite - Pixbob Lite from Habib Khoirul Fajar
+]]
 BASE_FONT = love.graphics.newFont('PixBob-Lite.ttf', 32)
 TOOLS_FONT = love.graphics.newFont('PixBob-Lite.ttf', 16)
 
-local updateNum 
+--global variable to keep track of the number of updates since the game started runing.
+local updateNum
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
